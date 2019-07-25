5334e99c5641f3fc9d36b5a0005d36f8fae2992f
Carl Kakisis <Messinias1@gmail.com>
2019-07-25 13:49:01 -0400

    closer to solution

--- week_1/cash.c
+++ week_1/cash.c
@@ -18,3 +18,57 @@ int main()
 
     return 0;
 }
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    float f;
+    do
+    {
+        f = get_float("Change: ");
+    }
+
+    while (f < 0.01 || f > 9999999);
+    f = (int)(f * 100);
+    //     printf("%.2f", f);
+    float q = .25;
+    float d = .1;
+    float n = .05;
+    float p = .01;
+
+    int count = 0;
+
+    while (q < f)
+    {
+        count++;
+        f - q;
+    }
+
+    while (.1 < f)
+    {
+        count++;
+        f - d;
+    }
+
+    while (n < f)
+    {
+        count++;
+        f - n;
+    }
+
+    while (.01 < f)
+    {
+        count++;
+        f - p;
+    }
+
+    printf("You will have %i coins returned back\n", count);
+
+    //     printf("F is %.2f and int is %d\n", f, (int)(f* 100));
+
+    return 0;
+}
