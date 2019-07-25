03880d747fe3ea73b0f4ae6b43c4e5554632f40c
Carl Kakisis <Messinias1@gmail.com>
2019-07-25 13:24:15 -0400

    float point conversion

--- week_1/cash.c
+++ week_1/cash.c
@@ -12,7 +12,9 @@ int main()
     }
 
     while (f < 0.01 || f > 9999999);
-    printf("F is %.2f and int is %d\n", f, (int)(f * 100));
+    f = (int)(f * 100);
+    //     printf("%.2f", f);
+    //     printf("F is %.2f and int is %d\n", f, (int)(f* 100));
 
     return 0;
 }
