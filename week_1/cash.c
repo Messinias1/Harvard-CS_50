4cb0cda6204db51141bc171788345dc401b67274
Carl Kakisis <Messinias1@gmail.com>
2019-07-26 14:03:19 -0400

    reformatted and better graded

--- week_1/cash.c
+++ week_1/cash.c
@@ -4,56 +4,57 @@
 #include <math.h>
 
 int main()
-    // Prompt user for input
+// Prompt user for input
 {
     float total;
-    do 
+    do
     {
         total = get_float("Change: ");
     }
-    
-    while (total < 0.01 || total > 9999999);
-    
+
+    while (total < 0);
+
     // Round to 100 and create change variables
-    
-    total = (int)(total* 100);
+
+    total = roundf(total * 100);
+    printf("%.2f\n", total);
 
     float q = 25;
     float d = 10;
     float n = 5;
     float p = 1;
-    
+
     int count = 0;
-    
+
     // Increase coin count and subract total while true
-    
-    while (q <= total) 
+
+    while (q <= total)
     {
         count++;
         total = total - q;
     }
-    
-        while (d <= total) 
+
+    while (d <= total)
     {
         count++;
         total = total - d;
     }
-    
-        while (n <= total) 
+
+    while (n <= total)
     {
         count++;
         total = total - n;
     }
-    
-        while (p < total) 
+
+    while (p <= total)
     {
         count++;
         total = total - p;
     }
-    
+
     // Return count to user
-    
+
     printf("You will have %i coins returned back\n", count);
-    
+
     return 0;
 }
