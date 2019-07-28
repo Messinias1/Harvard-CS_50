44fd5c8ae06217df3f5604d37b26535e6ef94982
Carl Kakisis <Messinias1@gmail.com>
2019-07-27 21:42:14 -0400

    caesar solution

--- week_2/caesar.c
+++ week_2/caesar.c
@@ -1,5 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -8,8 +11,29 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    else
+
+    int key = atoi(argv[1]);
+    if (key < 0)
     {
-        printf("Success\n");
+        printf("Key must be a positive number\n");
+        return 1;
     }
+
+    string plaintext = get_string("plaintext: ");
+    int len = strlen(plaintext);
+
+    printf("ciphertext: ");
+
+    for (int i = 0; i < len; i++)
+    {
+        if (islower(plaintext[i]))
+            printf("%c", (plaintext[i] - 'a' + key) % 26 + 'a');
+
+        else if (isupper(plaintext[i]))
+            printf("%c", (plaintext[i] - 'A' + key) % 26 + 'A');
+
+        else
+            printf("%c", plaintext[i]);
+    }
+    printf("\n");
 }
