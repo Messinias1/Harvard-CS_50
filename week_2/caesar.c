cc1a397d28693c7573ea62ee3905667e300c2dd8
Carl Kakisis <Messinias1@gmail.com>
2019-07-27 20:00:14 -0400

    caesar test for terminal command count

---
+++ week_2/caesar.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    else
+    {
+        printf("Success\n");
+    }
+}
