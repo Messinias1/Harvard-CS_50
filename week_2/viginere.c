b6dc1a4aa1cbf91159acb248548d33bdaa59c426
Carl Kakisis <Messinias1@gmail.com>
2019-07-29 13:57:58 -0400

    check character

--- week_2/viginere.c
+++ week_2/viginere.c
@@ -1,6 +1,25 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int shift(char c);
 
 int main(int argc, string argv[])
 {
+    char c;
+    printf("Enter a character: ");
+    scanf("%c", &c);
+    if (isalpha(c) == 0)
+        printf("Usage: ./vigenere keyword");
+    else
+        printf("Success");
+    return 0;
+}
+
+int shift(char c)
+{
+    int key = shift(argv[1][0]);
+    printf("%i\n", key);
 }
