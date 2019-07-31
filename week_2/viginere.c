67284f99a535565e6821603989c1ca9ce7aca143
Carl Kakisis <Messinias1@gmail.com>
2019-07-31 00:15:02 -0400

    viginere solution

--- week_2/viginere.c
+++ week_2/viginere.c
@@ -4,22 +4,52 @@
 #include <string.h>
 #include <ctype.h>
 
-int shift(char c);
-
 int main(int argc, string argv[])
 {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c", &c);
-    if (isalpha(c) == 0)
-        printf("Usage: ./vigenere keyword");
-    else
-        printf("Success");
-    return 0;
-}
+    // Declare if argc isnt equal to 2 end program
+    if (argc != 2)
+    {
+        printf("Usage: ./viginere <keyword>\n");
+        return 1;
+    }
 
-int shift(char c)
-{
-    int key = shift(argv[1][0]);
-    printf("%i\n", key);
+    // Make argv[1] a string loop in array and see if key is alphabetical
+    string key = argv[1];
+    int lenk = strlen(key);
+
+    for (int i = 0; i < lenk; i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            printf("keyword must be letters only\n");
+            return 1;
+        }
+    }
+
+    // Get user plaintext iterate through plaintext repeat j for lenk of key, print ciphertext
+    string plaintext = get_string("plaintext: ");
+    int len = strlen(plaintext);
+
+    printf("ciphertext: ");
+
+    for (int i = 0, j = 0; i < len; i++)
+    {
+        if (isalpha(plaintext[i]))
+        {
+            if (islower(plaintext[i]))
+            {
+                printf("%c", (plaintext[i] - 'a' + toupper(key[j]) - 'A') % 26 + 'a');
+            }
+            else if (isupper(plaintext[i]))
+            {
+                printf("%c", (plaintext[i] - 'A' + toupper(key[j]) - 'A') % 26 + 'A');
+            }
+            j = (j + 1) % lenk;
+        }
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
 }
