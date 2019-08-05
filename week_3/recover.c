6e54fb38a77770c2ae7ae831f1449e5cb8887e20
Carl Kakisis <Messinias1@gmail.com>
2019-08-05 11:53:21 -0400

    recover solution

--- week_3/recover.c
+++ week_3/recover.c
@@ -1,10 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BLOCK_SIZE 512
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+    // Check argument count
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
+        fprintf(stderr, "Usage: ./recover  Enter image FILE\n");
+        return 1;
     }
+
+    // remember filenames
+    char *infile = argv[1];
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", infile);
+        return 2;
+    }
+
+    BYTE buffer[512];
+    int imgCount = 0;
+
+    char filename[8];
+    FILE *outptr = NULL;
+
+    while (true)
+    {
+        // read a block of the memory card image
+        size_t bytesRead = fread(buffer, sizeof(BYTE), BLOCK_SIZE, inptr);
+
+        // break out of the loop when we reach the end of the card image
+        if (bytesRead == 0 && feof(inptr))
+        {
+            break;
+        }
+
+        // check if we found a JPEG
+        bool containsJpegHeader = buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+
+        // if we found a yet another JPEG, we must close the previous file
+        if (containsJpegHeader && outptr != NULL)
+        {
+            fclose(outptr);
+            imgCount++;
+        }
+
+        // if we found a JPEG, we need to open the file for writing
+        if (containsJpegHeader)
+        {
+            sprintf(filename, "%03i.jpg", imgCount);
+            outptr = fopen(filename, "w");
+        }
+
+        // write anytime we have an open file
+        if (outptr != NULL)
+        {
+            fwrite(buffer, sizeof(BYTE), bytesRead, outptr);
+        }
+    }
+
+    // close infile
+    fclose(inptr);
+
+    // close outfile
+    fclose(outptr);
+
+    // success
+    return 0;
 }
