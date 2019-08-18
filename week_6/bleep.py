a9ae67aaae9f61760f5c61ad028f9be8f9397f2f
Carl Kakisis <Messinias1@gmail.com>
2019-08-18 15:43:41 -0400

    bleep solution

--- week_6/bleep.py
+++ week_6/bleep.py
@@ -9,20 +9,27 @@ def main():
         print("Usage: python bleep.py file")
         exit(1)
 
-    userInput = get_string("What line would you like to censor? \n")
-
-    path = "banned.txt"
+    path = argv[1]
     banned_file = open(path,'r')
     file_word_list = banned_file.read()
+
+    userInput = get_string("What line would you like to censor? \n")
 
     inputList = userInput.split()
     print(inputList)
     # print(file_word_list)
 
+    censorship = ""
+
     for word in inputList:
         if word.lower() in file_word_list:
             hashed = "*" * len(word)
-            print(userInput.replace(word, hashed))
+            censorship += hashed + " "
+        else:
+            censorship += word + " "
+            # print(userInput.replace(word, hashed))
+
+    print(censorship.strip())
 
 
 if __name__ == "__main__":
