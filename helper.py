17e9c2bd00a52dcb4d87a322bf799b70f20543ba
Carl Kakisis <Messinias1@gmail.com>
2019-08-20 13:41:57 -0400

    substring solution

--- helper.py
+++ helper.py
@@ -27,4 +27,16 @@ def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
 
     # TODO
+    file1_sub = set()
+    file2_sub = set()
+
+    for i in range(len(a) - n + 1):
+        file1_sub.add(a[i:i + n])
+        # print(file1_sub)
+    for i in range(len(b) - n + 1):
+        file2_sub.add(b[i:i + n])
+
+    print(file1_sub & file2_sub)
+
+
     return []
