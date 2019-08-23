777dca12d721d3f6c062f2f4bb6ef9629f6373f6
Carl Kakisis <Messinias1@gmail.com>
2019-08-23 13:24:40 -0400

    survey complete

--- week_7/application.py
+++ week_7/application.py
@@ -31,9 +31,33 @@ def get_form():
 
 @app.route("/form", methods=["POST"])
 def post_form():
-    return render_template("error.html", message="TODO")
+    if not request.form.get("Name") or not request.form.get("Faction") or not request.form.get("Role"):
+        return render_template("error.html", message="Please fill in all fields before submission!!")
+
+    with open('survey.csv', 'w') as f:
+        fieldnames = ['Name', 'Faction', 'Role']
+        thewriter = csv.DictWriter(f, fieldnames=fieldnames)
+
+        thewriter.writeheader()
+        thewriter.writerow({'Name' : 'Bill', 'Faction' : 'NCR', 'Role' : 'Sniper'})
+        thewriter.writerow({'Name' :  request.form.get("Name"), 'Faction' :  request.form.get("Faction"), 'Role' :  request.form.get("Role")})
+
+    return redirect("/sheet")
+    # return render_template("error.html", message="TODO")
 
 
 @app.route("/sheet", methods=["GET"])
 def get_sheet():
-    return render_template("error.html", message="TODO")
+    with open('survey.csv') as file:
+        reader = csv.DictReader(file)
+        data = list(reader)
+        # line_count = 0
+        # for row in reader:
+        #     if line_count == 0:
+        #         print(f'{", ".join(row)}')
+        #         line_count += 1
+        #     print(f'{row["Name"]}  {row["Faction"]}  {row["Role"]}.')
+        #     line_count += 1
+        # print(f'Processed {line_count} lines.')
+
+    return render_template("sheet.html", data = data)
