ef36efb6a8df87abf0c732ee648f8c7f4fb12446
Carl Kakisis <Messinias1@gmail.com>
2019-08-27 01:07:05 -0400

    register route complete

--- finance/application.py
+++ finance/application.py
@@ -125,7 +125,57 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                      username=request.form.get("username"))
+
+        # Render an apology if the user’s input is blank
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Check if username already exists
+        elif len(rows) == 1:
+            return apology("Username already exists", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure confirmation was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must provide password confirmation", 403)
+
+        # Check if passwords match
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if password != confirmation:
+            return apology("passwords do not match", 403)
+
+        username = request.form.get("username")
+
+        # INSERT the new user into users, storing a hash of the user’s password, not the password itself. Hash the user’s password with generate_password_hash.
+
+        hash = generate_password_hash(password)
+        print(username)
+        print(hash)
+
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=hash)
+
+        # Remember which user has logged in
+        # session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
