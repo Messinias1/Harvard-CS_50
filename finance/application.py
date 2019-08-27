ac7fe53a5b5a77501a957f0ebb3f15c0d60243e3
Carl Kakisis <Messinias1@gmail.com>
2019-08-27 10:59:37 -0400

    successfully registered and saved user

--- finance/application.py
+++ finance/application.py
@@ -89,12 +89,15 @@ def login():
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
 
+        # print(rows)
+
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
+        print(rows[0]["id"])
 
         # Redirect user to home page
         return redirect("/")
@@ -132,17 +135,9 @@ def register():
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = :username",
-                      username=request.form.get("username"))
-
         # Render an apology if the user’s input is blank
         if not request.form.get("username"):
             return apology("must provide username", 403)
-
-        # Check if username already exists
-        elif len(rows) == 1:
-            return apology("Username already exists", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
@@ -159,18 +154,27 @@ def register():
         if password != confirmation:
             return apology("passwords do not match", 403)
 
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                      username=request.form.get("username"))
+
+        # Check if username already exists
+        if len(rows) == 1:
+            return apology("Username already exists", 403)
+
         username = request.form.get("username")
 
-        # INSERT the new user into users, storing a hash of the user’s password, not the password itself. Hash the user’s password with generate_password_hash.
-
         hash = generate_password_hash(password)
-        print(username)
-        print(hash)
+        # print(username)
+        # print(hash)
 
         db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash)", username=username, hash=hash)
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                      username=request.form.get("username"))
+        print(rows)
 
-        # Remember which user has logged in
-        # session["user_id"] = rows[0]["id"]
+        # Remember which user has registered
+        session["user_id"] = rows[0]["id"]
 
         # Redirect user to home page
         return redirect("/")
